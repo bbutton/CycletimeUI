655c76c49045154b288eaba95f7d562dc9b82f7b
Brian Button <brian.button@ctl.io>
2016-04-18 09:53:12 -0500

    Deleted unused file

--- lib/cycletime_repository.rb
+++ lib/cycletime_repository.rb
@@ -11,13 +11,13 @@ class CycletimeRepository
     app = Orchestrate::Application.new(@api_key, @endpoint)
     data = app[@collection]
 
-    metrics = data.search("board_id: #{board_id}")
+    metrics = data.search("board_id: #{board_id}").order(:end_time, :asc)
     return 200 if metrics == []
 
     cycle_times = metrics.find.collect do |m|
       score = m[0]
       metric = m[1].value
-      {card: metric["id"], cycle_time: metric["cycle_time"], estimate: metric["estimate"]}
+      {card: metric["id"], name: metric["name"], cycle_time: metric["cycle_time"], estimate: metric["estimate"], end_time: metric["end_time"]}
     end
 
     cycle_times
